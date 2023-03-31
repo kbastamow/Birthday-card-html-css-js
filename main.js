039455537eeb8fbe93e7c6f99d3cf1888f26ea04
kbastamow <bastamowkat@gmail.com>
2023-03-31 22:09:02 +0200

    animation and backflip done

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
 const starGround = document.getElementById("starGround");
-
-
-
+const beginAgain = document.getElementById("back")
 const palette = ["#9ad2cbff", "#d7ebbaff", "#faa613ff", "#f44708ff", "#a10702ff"];  
-
+let count = 0;  //for the animation
+let sec = 1000; //interval starts at 1s
 
 function randomColor(array) {
     let color = Math.floor(Math.random() * array.length);
-    console.log("HEre");
     return array[color];
-
 }
 
 function randomPos(){
-    return Math.floor(Math.random()* 301)
+    return Math.floor(Math.random()* 280)
 }
 
 
-// let intervalTime = 1000;
-// let animation = setInterval(makeStar, 50)
-// let reduceInterval =setInterval(reduce, 1000)
+let animation = setInterval(makeStar, sec) 
 
 function makeStar() {
     let star = document.createElement("div")
     star.innerHTML = `<i class="fa-solid fa-star fa-xl" style="color: ${randomColor(palette)}; position:absolute; right:${randomPos()}px"></i>`
     starGround.appendChild(star);
+    count++;
+    if (sec <= -5000) {  //stop animation after starburst
+        sec = 1000;
+        count = 0;
+        clearInterval(animation);  
+    }
+    if (count === 5) { //every 5 rounds, the interval gets reduced
+        sec -= 100
+        count = 0
+        clearInterval(animation);  //Clears the interval completely
+        animation = setInterval(makeStar, sec); //starts it again,  now with less time
+    }
 }
 
-// function reduce() {
-//     intervalTime -= 50;
-//     if (intervalTime <= 0) {
-//         clearInterval(animation);
-//       }
-// }
-
+beginAgain.addEventListener('click', function() {
+  location.reload();
+});
 
